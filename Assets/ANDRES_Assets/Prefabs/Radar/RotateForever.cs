fc21fb2f3ab74a2e51d196e34fb59c90f3af7601
asr <ajsoriar@gmail.com>
2023-05-13 01:37:52 +0200

    Fixes related to the player and new radar component

---
+++ Assets/ANDRES_Assets/Prefabs/Radar/RotateForever.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.Port;
+
+public class RotateForever : MonoBehaviour
+{
+
+
+    float speed = -200f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.Self);
+    }
+
+    /*
+    public float rotationSpeed;
+    public Vector3 rotationAxis = Vector3.up;
+    public Transform pivotPoint;
+
+    private void Start()
+    {
+        if (rotationSpeed < 100) { 
+            rotationSpeed = -200f;
+        }
+        
+        if (pivotPoint == null)
+        {
+            pivotPoint = transform;
+        }
+
+        StartCoroutine(RotateObject());
+    }
+
+    private IEnumerator RotateObject()
+    {
+        while (true)
+        {
+            //transform.RotateAround(pivotPoint.position, rotationAxis, rotationSpeed * Time.deltaTime);
+            transform.RotateAround(pivotPoint.localPosition, rotationAxis, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    */
+
+}
+
+/*
+public class RotateForever : MonoBehaviour
+{
+    public float rotationSpeed = -200f;
+    public Vector3 rotationAxis = Vector3.up;
+    public Transform pivotPoint;
+
+    private void Start()
+    {
+        if (pivotPoint == null)
+        {
+            pivotPoint = transform;
+        }
+
+        StartCoroutine(RotateObject());
+    }
+
+    private IEnumerator RotateObject()
+    {
+        while (true)
+        {
+            transform.RotateAround(pivotPoint.localPosition, rotationAxis, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
+
+*/
